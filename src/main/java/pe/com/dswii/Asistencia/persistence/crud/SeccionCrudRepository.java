ce4f22c60062cc1a8385eabd7c8a069f0cf402cb
hapastorv <alepastor1@hotmail.com>
2023-11-06 17:42:15 -0500

    Agregando consultas.

--- src/main/java/pe/com/dswii/Asistencia/persistence/crud/SeccionCrudRepository.java
+++ src/main/java/pe/com/dswii/Asistencia/persistence/crud/SeccionCrudRepository.java
@@ -2,12 +2,14 @@ package pe.com.dswii.Asistencia.persistence.crud;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pe.com.dswii.Asistencia.persistence.entity.Seccion;
 import java.util.List;
 import java.util.Optional;
 
 public interface SeccionCrudRepository extends JpaRepository<Seccion, Integer> {
-    Optional<List<Seccion>> findByNombreSeccion(String nombreSeccion);
+    @Query(value = "SELECT * FROM tb_seccion WHERE nombre_seccion LIKE %:nombreSeccion% AND activo_seccion = 'A'", nativeQuery = true)
+    Optional<List<Seccion>> findByNombreSeccion(@Param("nombreSeccion") String nombreSeccion);
     @Query(value = "SELECT * FROM tb_seccion WHERE activo_seccion = 'A'", nativeQuery = true)
     Optional<List<Seccion>> findAllActive();
     @Query(value = "SELECT * FROM tb_seccion WHERE activo_seccion = 'I'", nativeQuery = true)
