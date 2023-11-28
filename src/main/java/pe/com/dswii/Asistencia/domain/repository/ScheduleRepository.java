4bd69e89a20af9ca953dec91c29ebaa6b2c245ce
Nemo <torrestjfelix@gmail.com>
2023-11-28 11:51:28 -0500

    Agregando el schedule reporitory en el domain

---
+++ src/main/java/pe/com/dswii/Asistencia/domain/repository/ScheduleRepository.java
@@ -0,0 +1,9 @@
+package pe.com.dswii.Asistencia.domain.repository;
+
+import pe.com.dswii.Asistencia.domain.Schedule;
+
+import java.util.List;
+
+public interface ScheduleRepository {
+    Schedule save(Schedule schedule);
+}
