4e8382b91fff05a4c82dce74429904a7196903ab
Nemo <torrestjfelix@gmail.com>
2023-11-28 11:49:21 -0500

    cambios al schedule mapper para ignorar atributos quitados momentaneamente

--- src/main/java/pe/com/dswii/Asistencia/persistence/mapper/ScheduleMapper.java
+++ src/main/java/pe/com/dswii/Asistencia/persistence/mapper/ScheduleMapper.java
@@ -8,7 +8,7 @@ import pe.com.dswii.Asistencia.persistence.entity.Horario;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring", uses = {CourseMapper.class, PersonMapper.class, SectionMapper.class})
+@Mapper(componentModel = "spring", uses = {ScheduleDetailMapper.class,CourseMapper.class, PersonMapper.class, SectionMapper.class})
 public interface ScheduleMapper {
 
     @Mapping(source = "idHorario", target = "scheduleId")
@@ -16,10 +16,14 @@ public interface ScheduleMapper {
     @Mapping(source = "idSeccion", target = "sectionId")
     @Mapping(source = "idProfesor", target = "teacherId")
     @Mapping(source = "activoHorario", target = "scheduleActive")
+    @Mapping(source = "detalles", target = "details")
     Schedule toSchedule(Horario horario);
 
     List<Schedule> toSchedules(List<Horario> horarios);
 
     @InheritInverseConfiguration
+    @Mapping(target = "objCurso", ignore = true)
+    @Mapping(target = "objSeccion", ignore = true)
+    @Mapping(target = "objPersona", ignore = true)
     Horario toHorario(Schedule schedule);
 }
